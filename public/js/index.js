9c2be6146c694b096f5a347ad4351934eaf4ea06
mosuke5 <g.vamos603@gmail.com>
2015-01-25 09:11:06 +0000

    ・設定項目の追加(html側) ・JS側のJSON形式を変更

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,46 @@
 //vimrc設定ボタンを押した際の動作
 $("#setting-btn").click(function(event){
-  
-  var results = [];
+  /*
+   * json形式
+  {
+    "filepath": "/path/to/.vimrc",
+    "vimrc_contents": {
+      "number": true,     // set number
+      "ruler": false,     // set noruler
+      "history": 100,     // set history=100
+      "encoding": "utf-8" // set encoding=utf-8
+    }
+  }
+  */
+
+  var results = {};
    
-  $(".vimrc-contents:checked").map(function(){
+  $(".vimrc-contents").map(function(){
   
-    results.push($(this).attr('name'));
+    var key = $(this).attr('name');
+    var val; 
+
+    //チェックボックスなら
+    if($(this).attr('type')=='checkbox'){
+
+      if($(this).prop('checked')){
+        val = true;
+      }else{
+        val = false;
+      }
+    
+    //それ以外(input type=text)
+    }else{
+      val = $(this).val();
+    }
+
+    results[key] = val;
 
   });
   
-  console.log(results);
+  //console.log(JSON.stringify({"filepath": "/tmp/hoge.txt", "contents": results}))
   
+   
   $.ajax({
     type: "POST",
     url: "http://192.168.33.100:9292/api/vimrc",
@@ -26,6 +56,7 @@ $("#setting-btn").click(function(event){
       console.log(data);
     },
   });
+  
   
 });
 
